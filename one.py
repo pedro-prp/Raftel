34b6c1e65ead8e780bc44bf4d7826930b606e6af
Guilherme <marquesguilherme3@gmail.com>
2020-01-23 18:31:54 -0300

    Split functions

--- one.py
+++ one.py
@@ -1,27 +1,60 @@
 import requests
-import os
 from pathlib import Path
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-manga_number = 1
-browser.get(f'https://onepieceex.net/mangas/leitor/{manga_number}/')
-num_page = browser.find_element_by_xpath('//*[@id="mangapaginas"]')
-num_page = num_page.find_elements_by_tag_name("li")
 
-chap_path = Path(f'./chapters/{manga_number}')
-if not chap_path.exists():
-    chap_path.mkdir(parents=True, exist_ok=True)
-for i in range(len(num_page)-1):
-    num_page[i].click()
-    str_src = browser.find_element_by_xpath(
-        '/html/body/div[1]/div[3]/div[4]/a/img'
-    )
-    src = str_src.get_attribute('outerHTML').split('src=')[1].split('"')[1]
-    ext = src.split('.')[-1]
-    url = f'https://onepieceex.net' + src
-    mfile = requests.get(url, allow_redirects=True)
-    page = chap_path / f'{i+1}.{ext}'
-    page.open(mode='wb').write(mfile.content)
+def get_chapter_pages(browser: webdriver, chapter: int) -> int:
+    """Get the number of pages in the specified chapter"""
+    browser.get(f'https://onepieceex.net/mangas/leitor/{chapter}/')
+    pages = browser.find_element_by_xpath('//*[@id="mangapaginas"]')
+    return pages.find_elements_by_tag_name("li")
+    
 
-browser.close()
+def build_chapter_directory(chapter: int) -> Path:
+    """Build directory structure to chapter"""
+    chapter_path = Path(f'./chapters/{chapter}')
+    if not chapter_path.exists():
+        chapter_path.mkdir(parents=True, exist_ok=True)
+
+    return chapter_path
+
+
+def save_page(
+    chapter_path: Path,
+    page_url: str,
+    page_number: int,
+    image_ext: str
+):
+    """Download page and save"""
+    mime_file = requests.get(page_url, allow_redirects=True)
+    page = chapter_path / f'{page_number}.{image_ext}'
+    page.open(mode='wb').write(mime_file.content)
+
+
+def iter_pages(
+    browser: webdriver,
+    pages: list
+):
+    """Yields page data"""
+    for i in range(len(pages)-1):
+        pages[i].click()
+        raw_src = browser.find_element_by_xpath(
+            '/html/body/div[1]/div[3]/div[4]/a/img'
+        )
+        src = raw_src.get_attribute('outerHTML').split('src=')[1].split('"')[1]
+        ext = src.split('.')[-1]
+        url = f'https://onepieceex.net' + src
+        page_number = i + 1
+        yield (url, page_number, ext)
+
+
+if __name__ == '__main__':
+    browser = webdriver.Firefox()
+    manga_number = 5
+
+    chapter_path = build_chapter_directory(manga_number)
+    pages = get_chapter_pages(browser, manga_number)
+    for page_data in iter_pages(browser, pages):
+        save_page(chapter_path, *page_data)
+
+    browser.close()
