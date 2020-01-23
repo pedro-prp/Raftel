e2753362a8985a7870bd99b10edd2903f6960e27
Pedro Rodrigues <peddroprp@gmail.com>
2020-01-23 16:40:27 -0300

    Add flag arg to download all chapters

--- one.py
+++ one.py
@@ -2,39 +2,21 @@ from selenium.webdriver.firefox.options import Options
 from selenium import webdriver
 import argparse
 import requests
+import sys
 import os
 
 
 def main():
     args = build_args()
 
-    manga_number = vars(args)['chapter_number']
-
     browser = build_browser()
 
-    browser.get(f'https://onepieceex.net/mangas/leitor/{manga_number}/')
+    if vars(args)['all']:
+        download_all(browser)
+    else:
+        chapter = vars(args)['chapter_number']
 
-    pages = browser.find_element_by_xpath('//*[@id="mangapaginas"]')
-    pages = pages.find_elements_by_tag_name("li")
-
-    try:
-        os.mkdir('cap-' + str(manga_number))
-
-        for i in range(len(pages)-1):
-            pages[i].click()
-
-            src = build_img_src(browser)
-
-            url = f'https://onepieceex.net' + src
-
-            path = build_path(src, manga_number, i)
-
-            media = requests.get(url, allow_redirects=True)
-
-            open(path, 'wb').write(media.content)
-
-    except Exception:
-        print('already downloaded')
+        download_one(browser, chapter)
 
     browser.close()
 
@@ -43,8 +25,47 @@ def build_browser():
     options = Options()
     options.headless = True
     browser = webdriver.Firefox(options=options)
+    # browser = webdriver.Firefox()
 
     return browser
+
+
+def download_all(browser):
+    for i in range(1, 4):
+        download_one(browser, i)
+
+
+def download_one(browser, chapter):
+    print(f'Downloading chapter {chapter}')
+
+    try:
+        os.mkdir('cap-' + str(chapter))
+
+        browser.get(f'https://onepieceex.net/mangas/leitor/{chapter}/')
+
+        pages = browser.find_element_by_xpath('//*[@id="mangapaginas"]')
+        pages = pages.find_elements_by_tag_name("li")
+
+        for i in range(len(pages)-1):
+            print('.', end='')
+            sys.stdout.flush()
+
+            pages[i].click()
+
+            src = build_img_src(browser)
+
+            url = f'https://onepieceex.net' + src
+
+            path = build_path(src, chapter, i)
+
+            media = requests.get(url, allow_redirects=True)
+
+            open(path, 'wb').write(media.content)
+
+        print('')
+
+    except Exception:
+        print('already downloaded')
 
 
 def build_args():
@@ -55,6 +76,10 @@ def build_args():
                       action='store',
                       type=int,
                       dest='chapter_number')
+
+    args.add_argument('-a',
+                      '--all',
+                      action='store_true')
 
     args = args.parse_args()
 
