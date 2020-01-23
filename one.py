25b40a857b66fdd61d289c893f54b51c435957a2
Guilherme <marquesguilherme3@gmail.com>
2020-01-23 16:12:38 -0300

    remove unnecessary try/except

--- one.py
+++ one.py
@@ -9,23 +9,19 @@ browser.get(f'https://onepieceex.net/mangas/leitor/{manga_number}/')
 num_page = browser.find_element_by_xpath('//*[@id="mangapaginas"]')
 num_page = num_page.find_elements_by_tag_name("li")
 
-try:
-    chap_path = Path(f'./chapters/{manga_number}')
-    if not chap_path.exists():
-        chap_path.mkdir(parents=True, exist_ok=True)
-    for i in range(len(num_page)-1):
-        num_page[i].click()
-        str_src = browser.find_element_by_xpath(
-            '/html/body/div[1]/div[3]/div[4]/a/img'
-        )
-        src = str_src.get_attribute('outerHTML').split('src=')[1].split('"')[1]
-        ext = src.split('.')[-1]
-        url = f'https://onepieceex.net' + src
-        mfile = requests.get(url, allow_redirects=True)
-        page = chap_path / f'{i+1}.{ext}'
-        page.open(mode='wb').write(mfile.content)
-except Exception as err:
-    print(err)
-    print('already downloaded')
+chap_path = Path(f'./chapters/{manga_number}')
+if not chap_path.exists():
+    chap_path.mkdir(parents=True, exist_ok=True)
+for i in range(len(num_page)-1):
+    num_page[i].click()
+    str_src = browser.find_element_by_xpath(
+        '/html/body/div[1]/div[3]/div[4]/a/img'
+    )
+    src = str_src.get_attribute('outerHTML').split('src=')[1].split('"')[1]
+    ext = src.split('.')[-1]
+    url = f'https://onepieceex.net' + src
+    mfile = requests.get(url, allow_redirects=True)
+    page = chap_path / f'{i+1}.{ext}'
+    page.open(mode='wb').write(mfile.content)
 
 browser.close()
